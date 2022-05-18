df8dd4cd4214bdf918703f8e85cc0c3c1b2ee977
xmit <xmit@xmitdeiMac.lan>
2022-05-19 07:32:07 +0800

    mdict init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
 import "./App.css";
 import { dialog } from "@tauri-apps/api";
+import Helmet from "react-helmet";
 
 function App() {
   return (
     <div className='App'>
-      <p
-        onClick={async (e) => {
-          const filePath = await dialog.open({
-            title: "请选择文件",
-          });
-          if (!filePath) {
-            return;
-          }
-          console.log(filePath);
-        }}
-      >
-        123
-      </p>
+      <input id='dictfile' type='file' multiple />
+      <input id='word' type='text' value='operation' />
+      <input id='btnLookup' type='button' value='look up' disabled='false' />
+      <div id='dict-title' />
+      <div id='definition' />
+      <Helmet>
+        <script src={process.env.PUBLIC_URL + "/static/js/conf.js"}></script>
+        <script
+          src={process.env.PUBLIC_URL + "/static/js/require.js"}
+          data-main={process.env.PUBLIC_URL + "/static/js/mdict.js"}
+        ></script>
+      </Helmet>
     </div>
   );
 }
