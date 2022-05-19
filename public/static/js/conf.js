6a611819800800f76ce46a56c30c890c254c1806
xmit <xmit@xmitdeiMac.lan>
2022-05-19 19:51:24 +0800

    mdict basis done

--- public/static/js/conf.js
+++ public/static/js/conf.js
@@ -7,22 +7,13 @@ var require = {
     jquery: "jquery-1.11.3.min",
     pako: "pako_inflate.min",
     lzo: "minilzo-decompress.min",
-    /*    
-    'lzo'           : 'lzo-wrapper',
-    'lzo1x'         : 'lzo1x', 
-    'lzo-wrapper'   : 'lzo-wrapper', 
-//*/
-
     bluebird: "bluebird.min",
     selectize: "selectize.min",
-
     speex: "speex.min",
     pcmdata: "pcmdata.min",
     bitstring: "bitstring.min",
-
     murmurhash3: "murmurhash3.min",
     ripemd128: "ripemd128.min",
-
     "mdict-commo": "mdict-common",
     "mdict-parser": "mdict-parser",
     "mdict-renderer": "mdict-renderer",
--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import React, { useEffect } from "react";
+import React, { useState, useCallback, useRef } from "react";
+import debounce from "lodash/debounce";
+
 import "./App.css";
-import { dialog } from "@tauri-apps/api";
-import Helmet from "react-helmet";
 
 function App() {
+  const [keyword, setKeyword] = useState("");
+  const [content, setContent] = useState("");
+  const mdictRef = useRef(null);
+
+  const getMdictMethods = (deps) => {
+    return new Promise((resolve, reject) => {
+      try {
+        window.requirejs([...deps], (...arg) => {
+          resolve(arg);
+        });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+
+  const initMdict = async (fileList) => {
+    try {
+      const [mParser, mRenderer] = await getMdictMethods([
+        "mdictParser",
+        "mdictRenderer",
+      ]);
+      const resources = await mParser(fileList);
+      const mdictRes = await mRenderer(resources);
+
+      return mdictRes;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDebounceSearch = async (keyword) => {
+    const content = await mdictRef.current.lookup(keyword);
+    setContent(content.html());
+  };
+
+  const debounceFn = useCallback(debounce(handleDebounceSearch, 300), []);
+
+  const keywordChange = (e) => {
+    const keywordVal = e?.target?.value || "";
+    setKeyword(keywordVal);
+    debounceFn(keywordVal);
+  };
+
   return (
     <div className='App'>
-      <input id='dictfile' type='file' multiple />
-      <input id='word' type='text' value='operation' />
+      <input
+        id='dictfile'
+        type='file'
+        multiple
+        onChange={async (e) => {
+          const mdict = await initMdict(e.target.files);
+          mdictRef.current = mdict;
+        }}
+      />
+      <input
+        id='keyword'
+        type='text'
+        value={keyword}
+        onChange={keywordChange}
+      />
       <input id='btnLookup' type='button' value='look up' disabled='false' />
-      <div id='dict-title' />
-      <div id='definition' />
-      <Helmet>
-        <script src={process.env.PUBLIC_URL + "/static/js/conf.js"}></script>
-        <script
-          src={process.env.PUBLIC_URL + "/static/js/require.js"}
-          data-main={process.env.PUBLIC_URL + "/static/js/mdict.js"}
-        ></script>
-      </Helmet>
+      <div id='definition' dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
 }
